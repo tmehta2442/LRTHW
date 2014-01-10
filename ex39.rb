6663d22810c06ea2c1a95a43d1ff15a2d594031b
tmehta2442 <tmehta@post.harvard.edu>
2014-01-10 00:59:40 -0800

    ex39

---
+++ ex39.rb
@@ -0,0 +1,24 @@
+ten_things = "Apple Oranges Crows Telephone Light Sugar"
+
+puts "wait there's not 10 things in that list, let;s fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = %w(Day NIght song Frisbee Corn Banana Girl Boy)
+
+while stuff.length != 10
+	next_one = more_stuff.pop()
+	puts "Adding: #{next_one}"
+	stuff.push(next_one)
+	puts "there's #{stuff.length} items now."
+end
+
+puts "there we go: #{stuff}"
+
+puts "Let's do some thigns with stuff."
+
+puts stuff[1]
+
+puts stuff[-1]
+puts stuff.pop()
+puts stuff.join(' ')
+puts stuff.values_at(3,5).join('#')
