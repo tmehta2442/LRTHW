5aec30f6e59c2d134231dd5cdc9f17a4c7745436
tmehta2442 <tmehta@post.harvard.edu>
2014-01-10 12:30:12 -0800

    ex40

---
+++ ex40.rb
@@ -0,0 +1,27 @@
+cities = { 'CA' => 'San Francisco',
+					 'MI' => 'Detriot',
+					 'FL' => 'Jacksonville'}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+#map varible can be anything
+def find_city(map, state)
+	if map.include? state
+		return map[state]
+	else
+		return "Not Found."
+	end
+end
+
+cities[:find] = method(:find_city)
+
+while true
+	print "State? (ENTER to quit) "
+	state = gets.chomp
+
+	break if state.empty?
+
+	# puts find_city(cities, state)
+	puts cities[:find].call(cities, state)
+end
